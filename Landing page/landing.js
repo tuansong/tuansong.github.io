257c685cd8001b028840331bccd7d6eb1dd1b721
tuansong <trantuansong22@gmail.com>
2018-05-14 15:19:53 -0700

    Almost done without spacing bug

---
+++ Landing page/landing.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    if($(window).width() > 1300) {
+        $(".d-block").addClass("w-100");
+    }
+    else {
+        $(".d-block").removeClass("w-100");
+    }
+    
+})
