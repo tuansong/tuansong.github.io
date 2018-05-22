486fdb763d2824ea01c5512f95a58fe01baabe89
tuansong <trantuansong22@gmail.com>
2018-05-22 10:25:47 -0700

    new update for portfolio

--- landing.js
+++ landing.js
@@ -2,9 +2,13 @@ $(document).ready(function(){
 
     if($(window).width() > 1300) {
         $(".d-block").addClass("w-100");
+        location.reload();
+        break;
     }
     else {
         $(".d-block").removeClass("w-100");
+        location.reload();
+        break;
     }
-    
+
 })
